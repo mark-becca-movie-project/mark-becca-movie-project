95c2ddd428ea3ecfdb7ed6f254ecada46b209b20
Michael Stokes <michael.stokes.347@gmail.com>
2022-04-05 00:00:16 -0500

    update

---
+++ requests.js
@@ -0,0 +1,15 @@
+
+
+//found from endpoints on the movie database
+const requests = {
+    fetchDocumentaries: `/discover/movie?api_key=${TMDB_API}&with_genres=99`,
+    fetchTrending: `/trending/all/week?api_key=${TMDB_API}&language=en-US`,
+    fetchNetflixOriginals: `/discover/tv?api_key=${TMDB_API}&with_networks=213`,
+    fetchTopRated: `/movie/top_rated?api_key=${TMDB_API}&language=en-US`,
+    fetchActionMovies: `/discover/movie?api_key=${TMDB_API}&with_genres=28`,
+    fetchComedyMovies: `/discover/movie?api_key=${TMDB_API}&with_genres=35`,
+    fetchHorrorMovies: `/discover/movie?api_key=${TMDB_API}&with_genres=27`,
+    fetchRomanceMovies: `/discover/movie?api_key=${TMDB_API}&with_genres=10749`
+}
+
+export default requests;
