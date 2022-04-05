3120b481384a9581a5b5a9b7caf102196dde7c83
Michael Stokes <michael.stokes.347@gmail.com>
2022-04-04 23:43:46 -0500

    version 2 changes

---
+++ keys.js
@@ -0,0 +1,4 @@
+const TMDB_API = "c2cc8e98bf7d78e509f32ca6ebc61519";
+const TMDB_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMmNjOGU5OGJmN2Q3OGU1MDlmMzJjYTZlYmM2MTUxOSIsInN1YiI6IjYyNGJiNDMzMWQxYmY0MDA2N2ZkZGVkZCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.hlAivpwQzfj1E5c3zXCZmDeVHVfSHJk_6yU2pf2-nSc";
+//     Example API Request:
+// https://api.themoviedb.org/3/movie/550?api_key=c2cc8e98bf7d78e509f32ca6ebc61519
