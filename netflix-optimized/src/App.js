cc7c2c7b3657858c9cf72e644afd423f3dad07bc
Michael Stokes <michael.stokes.347@gmail.com>
2022-04-06 08:09:34 -0500

    update to movie list, transform, transition, scrollbar hidden

--- netflix-optimized/src/App.js
+++ netflix-optimized/src/App.js
@@ -11,8 +11,12 @@ function App() {
         <div className="App">
             <h1>Netflix 2.0 (Testing by ReMark Studios)</h1>
             <h5>Powered by CodeUp Inspiration and Caffeine</h5>
-            <Row title="Comedies" fetchUrl={requests.fetchComedyMovies}/>
             <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+            <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+            <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+            <Row title="Comedies" fetchUrl={requests.fetchComedyMovies}/>
+            <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>  
+
             {/*{requests.}*/}
         </div>
     );
